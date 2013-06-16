5aa6f8898f17dd04967b91d757c42d42ebeecb44
Anthony Candaele <anthony.candaele@webmove.be>
2013-06-16 20:31:18 +0200

    add session management for user login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    # 1. got a User object
+    # 2. called authenticate method with password
+
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Welcome, you're logged in."
+    else
+      flash[:error] = "There is something wrong with your username or password"
+      redirect_to register_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Postit::Application.routes.draw do
   root to: 'posts#index'
 
   get '/register', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 
   resources :posts do
     resources :comments, only: [:create]
