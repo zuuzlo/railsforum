dc61d6dac9ff91402d76b025cf25f7b76802cfa0
Anthony Candaele <anthony.candaele@webmove.be>
2013-06-16 15:42:36 +0200

    styled new comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,13 @@
-require 'pry'
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.new(params[:comment])
-    @comment.user_id = 1
     @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(params[:comment])
+    @comment.user_id = 1 #TODO fix this after authentication
+    
     @comment.post_id = @post.id
     
       if @comment.save
-        redirect_to "/posts/#{@post.id}", notice: "comment was created"
+        redirect_to post_path(@post), notice: "Your post was added."
       else
         render "/posts/#{@post}/comments/new"
       end
