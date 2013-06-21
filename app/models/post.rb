4fc373b410e4098287d09f610d5f83d87ef3729e
Anthony Candaele <anthony.candaele@webmove.be>
2013-06-21 22:08:46 +0200

    implement total_votes method on post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,8 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :votes, as: :voteable
   validates :title, :url, presence: true
+
+  def total_votes
+    self.votes.where(vote: true).size - self.votes.where(vote: false).size
+  end
 end
