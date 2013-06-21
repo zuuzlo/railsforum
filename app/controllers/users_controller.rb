ca3519e2f1747becf81fd0b50877d4b7c74f41f6
Anthony Candaele <anthony.candaele@webmove.be>
2013-06-21 19:47:29 +0200

    add require_same_user helper method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_filter :require_same_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -11,6 +12,15 @@ class UsersController < ApplicationController
     else
       @user.errors.delete(:password_digest)
       render :new
+    end
+  end
+
+  private
+
+  def require_same_user
+    if current_user != User.find(params[:id])
+      flash[:error] = "You can't do that"
+      redirect_to root_path
     end
   end
 end
