daf8cb325bac8889b6329619d205ec242bb98910
Anthony Candaele <anthony.candaele@webmove.be>
2013-06-21 19:15:08 +0200

    create fix_url and display_datetime helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def fix_url(str) # "cnn.om" "http://cnn.com"
+    str.starts_with?("http://") ? str : "http://#{str}"
+  end
+
+  def display_datetime(dt)
+    dt.strftime("%m/%d%Y %l:%M%P") #02/12/2013 8:10pm
+  end
 end
