414a8f3334f82bd77d6343307d2f1755127e3817
Anthony Candaele <anthony.candaele@webmove.be>
2013-06-22 11:24:00 +0200

    implement voting on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_filter :require_user
+  before_filter :find_comment, only: [:create, :vote]
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(params[:comment])
@@ -11,5 +12,17 @@ class CommentsController < ApplicationController
       @post.comments.reload
       render 'posts/show'
     end    
+  end
+
+  def vote
+    Vote.create(voteable: @comment, user: current_user, vote: params[:vote])
+    flash[:notice] = "Your vote was counted"
+    redirect_to :back
+  end
+
+  private
+
+  def find_comment
+    @comment = Comment.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Postit::Application.routes.draw do
     member do
       post 'vote'
     end
-    resources :comments, only: [:create]
+    resources :comments, only: [:create] do
+      member do
+        post 'vote'
+      end
+    end
   end
   
   resources :users
